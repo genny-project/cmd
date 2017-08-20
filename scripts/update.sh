1d5fdff73f41e55e9da1c949476f4cf202babe22
andres <helios@andress-MBP.lan>
2017-08-21 05:25:12 +1000

    added fire-rule.json

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 VERSION=0.0.1
 
-jar uvf ../target/gebcmd-${VERSION}-SNAPSHOT-jar-with-dependencies.jar jsons/${1}.json
+cd jsons 
+jar uvf ../../target/gebcmd-${VERSION}-SNAPSHOT-jar-with-dependencies.jar ${1}.json
