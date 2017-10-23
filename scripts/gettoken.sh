ed9c3ed78d83eeac7fe790601e262336d2d77510
adamcrow64 <adamcrow63@gmail.com>
2017-10-24 09:36:16 +1100

    Updated realms in script to be genny

--- scripts/gettoken.sh
+++ scripts/gettoken.sh
@@ -15,7 +15,7 @@ if [ -z "${myip}" ]; then
 fi
 if [ -z "${1}" ]; then
 #printf "${RED}Getting OAuth2 token from Keycloak (includes access_token, refresh_token, etc):${NORMAL}\n"
-KEYCLOAK_RESPONSE=`curl -s -X POST http://${myip}:8180/auth/realms/wildfly-swarm-keycloak-example/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=curl'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
+KEYCLOAK_RESPONSE=`curl -s -X POST http://${myip}:8180/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=curl'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
 #printf "$KEYCLOAK_RESPONSE \n\n"
 
 #printf "${RED}Parsing access_token field, as we don't need the other elements:${NORMAL}\n"
