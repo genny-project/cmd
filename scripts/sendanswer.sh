9dd01cc7d3171ad0e39af2288cfd9bb7500221d3
adamcrow64 <adamcrow63@gmail.com>
2017-10-25 16:46:05 +1100

    Added sendanswer

--- scripts/sendanswer.sh
+++ scripts/sendanswer.sh
@@ -10,7 +10,7 @@ while IFS=$': \t' read -a line ;do
 if [ -z "${myip}" ]; then
    myip=127.0.0.1
 fi
-myip=localhost
+#myip=192.168.64.6
 
 KEYCLOAK_RESPONSE=`curl -s -X POST http://${myip}:8180/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=curl'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
 TOKEN=`echo "$KEYCLOAK_RESPONSE" | jq -r '.access_token'`
@@ -22,6 +22,6 @@ printf "${TOKEN} \n\n"
 #$CALLSCRIPT "$1"
 echo ""
 echo "curl -S -v -H 'Bearer: ${TOKEN}' http://${myip}:8280/qwanda/answers"
-curl -S -v  -k  -H "Authorization: Bearer $TOKEN"  "http://${myip}:8280/qwanda/answers"
+curl -X POST --header "Content-Type: application/json" -H "Authorization: Bearer $TOKEN"  --header "Accept: application/json" -d '{  "created": "2017-06-30T15:41:12",  "value": "Bob",  "expired": false,  "refused": false,  "weight": 1,  "targetCode": "PER_USER1",  "sourceCode": "PER_USER1",  "attributeCode": "PRI_FIRSTNAME"  }' "http://${myip}:8280/qwanda/answers"
 echo ""
 
