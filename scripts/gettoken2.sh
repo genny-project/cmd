9a9701e8ef449be3363fafe42db12667a2d64aea
Adam Crow <acrow@Adams-Compooter.local>
2018-01-21 20:49:34 +1100

    Got gps sim going

--- scripts/gettoken2.sh
+++ scripts/gettoken2.sh
@@ -2,11 +2,9 @@
 set -e
 
 keycloakUrl=$1
-#printf "${RED}Getting OAuth2 token from Keycloak (includes access_token, refresh_token, etc):${NORMAL}\n"
 KEYCLOAK_RESPONSE=`curl -s -X POST ${keycloakUrl}/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=genny'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
-#printf "$KEYCLOAK_RESPONSE \n\n"
 
 #printf "${RED}Parsing access_token field, as we don't need the other elements:${NORMAL}\n"
 ACCESS_TOKEN=`echo "$KEYCLOAK_RESPONSE" | jq -r '.access_token'`
-
+echo ${ACCESS_TOKEN}
 
