7a24ed818902b53bb90ec0adda7068764bf4fad4
Adam Crow <acrow@Adams-Compooter.local>
2018-04-13 11:16:58 +1000

    Updated test searchBes

--- scripts/hql.sh
+++ scripts/hql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HQLFILE=$1
 
-KEYCLOAK_RESPONSE=`curl -s -X POST http://keycloak.genny.live/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=genny'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
+KEYCLOAK_RESPONSE=`curl -s -X POST http://192.168.64.46:8180/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=user1' -d 'password=password1' -d 'grant_type=password' -d 'client_id=genny'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
 #printf "$KEYCLOAK_RESPONSE \n\n"
 
 #printf "${RED}Parsing access_token field, as we don't need the other elements:${NORMAL}\n"
