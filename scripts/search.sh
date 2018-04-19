61838eaec0fdeb69d2780d13ed3ee93d62df612d
Adam Crow <acrow@Adams-Compooter.local>
2018-04-19 18:20:47 +1000

    Updated test scripts

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BEFILE=$1
 #printf "${RED}Getting OAuth2 token from Keycloak (includes access_token, refresh_token, etc):${NORMAL}\n"
-KEYCLOAK_RESPONSE=`curl -s -X POST http://192.168.64.46:8180/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=admin' -d 'password=admin1' -d 'grant_type=password' -d 'client_id=genny'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
+KEYCLOAK_RESPONSE=`curl -s -X POST http://10.1.120.104:8180/auth/realms/genny/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=admin' -d 'password=admin1' -d 'grant_type=password' -d 'client_id=genny'  -d 'client_secret=056b73c1-7078-411d-80ec-87d41c55c3b4'`
 #printf "$KEYCLOAK_RESPONSE \n\n"
 
 #printf "${RED}Parsing access_token field, as we don't need the other elements:${NORMAL}\n"
